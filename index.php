15973a3208dd229e90141000317e69ec9b9b19fb
Tim <tim.hausman@yahoo.com>
2017-03-23 18:18:57 +0000

    initial

---
+++ index.php
@@ -0,0 +1,12 @@
+<html><body>
+ <?php
+
+
+?>   
+    <select name="formGender">
+<option value="">Select...</option>
+<option value="M">Male</option>
+<option value="F">Female</option>
+</select>
+</body></html>
+
